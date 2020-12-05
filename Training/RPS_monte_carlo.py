9934c6eeee1aca1d10ec3e42619872066e1f8f59
Yonah Aviv <68514914+y330@users.noreply.github.com>
2020-12-05 18:41:20 -0500

    Update and rename RPA-random-training.ipynb to RPS_monte_carlo.py

---
+++ Training/RPS_monte_carlo.py
@@ -0,0 +1,97 @@
+# author: Yonah Aviv
+# created: 11/09/2020
+# modified: 12/05/2020
+
+# training/simulations/test for rock paper scissors(rps)
+""" "Rock, Paper, Scissors game using a random number generator 
+to select two opponents for each round. Each game was played 
+to completion. To get smooth results, I repeated this exercise
+10,000 times for all combitions of starting players from 1-50 of
+each symbol. I counted the number of times the Rock team won, the 
+number of times the Paper team won, and the number of times the
+Scissor team won. I also recorded the average number of rounds that 
+were required to produce a winner"(DataGenetics)
+
+
+Rock, Paper, Scissors. Datagenetics.com (2013), (available at http://datagenetics.com/blog/march52013/index.html).
+"""
+
+
+import random
+import collections
+
+
+"""tables of outcomes for each hand:
+210
+021
+102
+"""
+shifted = [1,2]
+matrix = [2,1,0]
+matrix = collections.deque(matrix)
+wins = []
+
+choices = {
+    "rock": ["rock", "r", 0],
+    "paper": ["paper", "p", 1],
+    "scissors": ["scissors", "s", 2],
+}
+
+
+opponents = [AI(2) for i in range(10)]
+
+def AI(index):
+    return random.choice(list(choices.values()))[index]
+
+def GetKey(val):
+   for key, value in choice.items():
+      if val == value:
+         return key
+      return "key doesn't exist"
+
+def sample(size):
+    global selection 
+    selection = random.choices(population=opponents, k=size)
+    return selection
+def win(data, res):
+    global wins, shifted
+    for i in range(3):
+        if res == shifted:
+            print("3")
+            wins = (list(
+                        choices.keys()
+                    )
+                    [list
+                     (
+                         choices.values()
+                     ).index(
+                         [i][res[1]]
+                     )
+                    ]
+                   )
+            pass
+        matrix.rotate(-1)
+        shifted = list(matrix)[::2]
+        
+win(opponents, sample(2))
+    
+
+# def select(opponents):
+#     C=["rock","paper","scissors"]
+#     c,c1,p=random.choice(C),input(),{C[n]:C[(n+1)%3] for n in range(3)}
+#     if c1 in p:print("You chose {}\nI chose {}".format(c1,c));
+# CHOICES = ("rock", "paper", "scissors")
+# while True:
+#     human = raw_input("Your move [%s]: " % ",".join(CHOICES)).lower()
+#     if human not in CHOICES: continue
+#     cpu = random.choice(CHOICES)
+#     print("CPU plays: %s" % cpu)
+#     if human != cpu: break
+#     print("Tie, play again..\n")
+
+# print("You %s!" % ("Win" if CHOICES[CHOICES.index(human)-1] == cpu else "Lose"))
+
+# players = {}
+# players["opponents"] = enemys = [random.randint()] for i in range(10)]
+# for i in range(10):
+#   players["i"] = 4]
